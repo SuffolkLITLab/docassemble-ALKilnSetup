0de1d80df59c685718af2bbc2c914704a82fa344
Anonymous User <52798256+plocket@users.noreply.github.com>
2021-02-20 11:43:26 -0500

    Add ending page with some basic instructions

--- docassemble/ALAutomatedTestingTests/al_set_up_testing.py
+++ docassemble/ALAutomatedTestingTests/al_set_up_testing.py
@@ -8,19 +8,6 @@ from docassemble.base.util import log
 from docassemble.base.core import DAObject
 #from docassemble.base.util import DAFileList, DAFile
 
-# TODO:
-# √ Get username, reponame, personal access token
-# √ Get email, password, ... playground id from an interview url
-# Make secrets
-# Copy and transform files
-# Push to? New branch and PR? Or to default branch?
-
-# TODO validation:
-# Check repo address is sensical github address
-# Check user and repo exist
-# Check that user has permissions on the repo
-# Check that interview url is of correct type
-
 # reference:
 # https://gist.github.com/JeffPaine/3145490
 # https://docs.github.com/en/free-pro-team@latest/rest/reference/issues#create-an-issue
@@ -34,6 +21,46 @@ from docassemble.base.core import DAObject
 class TestInstaller(DAObject):
   def init( self, *pargs, **kwargs ):
     super().init(*pargs, **kwargs)
+  
+  def send_da_auth_secrets( self ):
+    """Set GitHub repo secrets the tests need to log into the da server and
+    create projects to run interviews to test."""
+    self.put_secret( secret_name='PLAYGROUND_EMAIL', secret_value=self.email )
+    self.put_secret( secret_name='PLAYGROUND_PASSWORD', secret_value=self.password )
+    self.set_da_server_info()
+    self.put_secret( secret_name='PLAYGROUND_ID', secret_value=self.playground_id )
+    return self
+  
+  def put_secret( self, secret_name='', secret_value='' ):
+    """Add one secret to the GitHub repo."""
+    # Convert the message and key to Uint8Array's (Buffer implements that interface)
+    encrypted_key = public.PublicKey( self.public_key.encode("utf-8"), encoding.Base64Encoder() )
+    sealed_box = public.SealedBox( encrypted_key )
+    # Encrypt using LibSodium.
+    encrypted = sealed_box.encrypt( secret_value.encode( "utf-8" ))
+    # Base64 the encrypted secret
+    base64_encrypted = b64encode( encrypted ).decode( "utf-8" )
+    #log( base64_encrypted, 'console' )
+    
+    secret_url = self.github_repo_base + "/actions/secrets/" + secret_name
+    secret_payload = '{"encrypted_value":"' + base64_encrypted + '", "key_id":"' + self.key_id + '"}'
+    secret_headers = {
+      'Accept': "application/vnd.github.v3+json",
+      'Authorization': self.basic_auth,
+    }
+    
+    secret_response = requests.request( "PUT", secret_url, data=secret_payload, headers=secret_headers )
+    #log( 'secret_response.text', 'console' )
+    #log( secret_response.text, 'console' )
+    
+    # TODO: Check there was no error
+    
+    # Cannot get the value of the secret to see if we're setting it correctly
+    # Can at least check that the secret exists
+    response = requests.request( "GET", secret_url, data="", headers=secret_headers )
+    log( response.text, 'console' )
+    
+    return self
 
   def set_github_auth( self ):
     """Set values needed for GitHub authorization.
@@ -69,58 +96,24 @@ class TestInstaller(DAObject):
     self.key_id = key_json[ 'key_id' ]
     self.public_key = key_json[ 'key' ]
     return self
-#  
-#  def put_secret( self, secret_name='', secret_value='' ):
-#    """Add one secret to the GitHub repo"""
-#    secret_name = 'FOO'
-#    secret_value = 'blah'
-#    # Convert the message and key to Uint8Array's (Buffer implements that interface)
-#    encrypted_key = public.PublicKey( self.public_key.encode("utf-8"), encoding.Base64Encoder() )
-#    sealed_box = public.SealedBox( encrypted_key )
-#    # Encrypt using LibSodium.
-#    encrypted = sealed_box.encrypt( secret_value.encode( "utf-8" ))
-#    # Base64 the encrypted secret
-#    base64_encrypted = b64encode( encrypted ).decode( "utf-8" )
-#    log( base64_encrypted, 'console' )
-#    
-#    secrets_url = self.github_repo_base + "/actions/secrets/" + secret_name
-#    secret_payload = '{"encrypted_value":"' + base64_encrypted + '", "key_id":"' + self.key_id + '"}'
-#    secret_headers = {
-#      'Accept': "application/vnd.github.v3+json",
-#      'Authorization': self.basic_auth,
-#    }
-#    
-#    secret_response = requests.request("PUT", secrets_url, data=secret_payload, headers=secret_headers)
-#    log( 'secret_response.text', 'console' )
-#    log( secret_response.text, 'console' )
-#    # Now get secrets val to check if it's right
-#    
-#    return self
-#  
-#  def send_secrets( self ):
-#    pass
-#  
-#  #def set_da_auth( self, email='', pword='' ):
-#  #  self.email = email
-#  #  self.password = password
-#  #  #return { "email": self.email, "password": self.password }
-#  #  return self
-#    
-#  def set_server_info( self, user_interview_url ):
-#    server_match = re.match( r"^(.+)\/interview\?i=docassemble\.playground", user_interview_url)
-#    if server_match is None:
-#      self.server_url = None
-#    else:
-#      self.server_url = server_match.group(1)
-#      
-#    id_match = re.match( r"^.+(?:interview\?i=docassemble\.playground)(\d+)(?:.*)$", user_interview_url )
-#    if id_match is None:
-#      self.playground_id = None
-#    else:
-#      self.playground_id = id_match.group(1)
-#      
-#    #return { "server_url": self.server_url, "playground_id": self.playground_id }
-#    return self
+    
+  def set_da_server_info( self ):
+    server_match = re.match( r"^(.+)\/interview\?i=docassemble\.playground", self.interview_url)
+    if server_match is None:
+      self.server_url = None
+    else:
+      self.server_url = server_match.group(1)
+      
+    id_match = re.match( r"^.+(?:interview\?i=docassemble\.playground)(\d+)(?:.*)$", self.interview_url )
+    if id_match is None:
+      self.playground_id = None
+    else:
+      self.playground_id = id_match.group(1)
+    
+    log( self.server_url, 'console' )
+    log( self.playground_id, 'console' )
+    #return { "server_url": self.server_url, "playground_id": self.playground_id }
+    return self
 #
 #  def get_files( self ):
 #    pass
