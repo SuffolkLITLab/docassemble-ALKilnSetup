e26b77a8e54279d990effeeb492213aaa3fd2a15
Anonymous User <52798256+plocket@users.noreply.github.com>
2021-02-23 09:36:20 -0500

    Break. Values of yml vars seem to get reset. Error?

--- docassemble/ALAutomatedTestingTests/al_set_up_testing.py
+++ docassemble/ALAutomatedTestingTests/al_set_up_testing.py
@@ -5,9 +5,9 @@ from nacl import encoding, public
 import codecs
 from base64 import b64encode
 import json
-from docassemble.base.util import log, zip_file
+from docassemble.base.util import log, zip_file, defined
 from docassemble.base.core import DAObject
-#from docassemble.base.util import DAFileList, DAFile
+from docassemble.base.util import DAFile, DAFileCollection
 
 # reference:
 # Mostly: https://pygithub.readthedocs.io/en/latest/introduction.html
@@ -32,6 +32,7 @@ class TestInstaller(DAObject):
   def send_da_auth_secrets( self ):
     """Set GitHub repo secrets the tests need to log into the da server and
     create projects to run interviews to test."""
+    # PyGithub cannot currently handle secrets
     self.set_github_auth()
     self.put_secret( secret_name='PLAYGROUND_EMAIL', secret_value=self.email )
     self.put_secret( secret_name='PLAYGROUND_PASSWORD', secret_value=self.password )
@@ -136,15 +137,20 @@ class TestInstaller(DAObject):
     
     return self
   
+  def set_repo( self ):
+    self.repo = self.github.get_user().get_repo( self.repo_name )
+  
   def create_branch( self ):
     # Get default branch
-    repo = self.github.get_user().get_repo( self.repo_name )
+    if not defined( 'installer.repo' ):
+      self.set_repo()
+    repo = self.repo
     default_branch_name = repo.default_branch
-    log( default_branch_name, 'console' )
     default_branch = repo.get_branch( default_branch_name )
     
-    self.ref_path = "refs/heads/automated_testing"  # path of new branch
-    ref_path = self.ref_path
+    self.branch_name = "automated_testing"
+    branch_name = self.branch_name
+    ref_path = "refs/heads/" + branch_name  # path of new branch
     count = 1
     max_count = 20
     while ( count < max_count ):
@@ -154,16 +160,16 @@ class TestInstaller(DAObject):
         response = repo.create_git_ref( ref_path, default_branch.commit.sha )
         break
       except Exception as error:
-        ref_path = self.ref_path + '_' + str( count )
+        branch_name = self.branch_name + '_' + str( count )
+        ref_path = "refs/heads/" + self.branch_name
         # Why does this make things get stuck on a previous page? (first page?)
         # Some kind of exception in here? Lets hope it doesn't occur at all.
         if count == max_count:
           # TODO: Tell the user to delete old branches
           installer.error = error.status # TODO: Make list of errors?
-          log( installer.error, 'console' )
     
-    self.ref_path = ref_path
-    log( self.ref_path, 'console' )
+    #self.ref_path = ref_path
+    self.branch_name = branch_name
     
     return self
   
@@ -180,6 +186,52 @@ class TestInstaller(DAObject):
     
     # Check mergability of a PR
     # https://docs.github.com/en/rest/guides/getting-started-with-the-git-database-api#checking-mergeability-of-pull-requests
+    
+    # > I'd just slurp the file contents into a string with open() and .read() and then use re.sub
+    
+    #log( self.run_interview_tests, 'console' )
+    #contents = self.run_interview_tests.slurp()
+    #log( 'slurp', 'console' )
+    #log( contents, 'console' )
+    
+    # Get the folder with the files
+    #files_repo = 
+    # https://raw.githubusercontent.com/plocket/docassemble-cucumber/main/pushing_testing_files/.env-example
+    
+    #if not defined( 'installer.repo' ):
+    #  self.set_repo()
+    #repo = self.repo
+    #
+    #files_to_push = repo.get_contents('pushing_testing_files')
+    #log( len(files_to_push), 'console' )
+    
+    #self.initializeAttribute('files', DAFileCollection)
+    
+    run_tests_str = self.run_interview_tests_str
+    #self.run_interview_tests = DAFile()
+    self.initializeAttribute('run_interview_tests', DAFile)
+    self.run_interview_tests.initialize( filename='run_interview_tests.yml', attachment=True )
+    self.run_interview_tests.write( run_tests_str )
+    log( self.run_interview_tests.extension, 'console' )
+    self.run_interview_tests.commit()
+    #self.files.appendObject( self.run_interview_tests )
+    
+    #gitignore_str = self.gitignore
+    ##self.gitignore = DAFile()
+    #self.initializeAttribute('gitignore', DAFile)
+    #self.gitignore.initialize( filename='.gitignore', attachment=True )
+    #self.gitignore.write( gitignore_str )
+    #log( self.gitignore.filename, 'console' )
+    #self.gitignore.commit()
+    #self.gitignore = 'blah'
+    
+    log( 'before', 'console' )
+    self.repo.create_file('file.yml', 'add file', run_tests_str, branch=self.branch_name)
+    log( 'sent', 'console' )
+    
+    return self
+  
+  def add_file_to_branch( self ):
     return self
 #
 #  def get_files( self ):
