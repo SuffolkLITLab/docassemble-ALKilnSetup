6406c957b93fbb484eb4dfd49883fa77637b57f4
Anonymous User <52798256+plocket@users.noreply.github.com>
2021-02-23 09:38:49 -0500

    Add pynacl dependency

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.ALAutomatedTestingTests',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['PyGithub>=1.54.1'],
+      install_requires=['PyGithub>=1.54.1', 'PyNaCl>=1.4.0'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ALAutomatedTestingTests/', package='docassemble.ALAutomatedTestingTests'),
      )
