c153af9ecd7fc0f7de22c83729647d357e33fad8
Anonymous User <52798256+plocket@users.noreply.github.com>
2021-07-17 06:38:34 -0400

    Implement all github functionality plus more validation

--- docassemble/ALAutomatedTestingTests/al_set_up_testing.py
+++ docassemble/ALAutomatedTestingTests/al_set_up_testing.py
@@ -65,84 +65,43 @@ class TestInstaller(DAObject):
       error1.data[ 'details' ] = self.github_token_error
       self.errors.append( error1 )
     
+    if self.user_name != '':
+      self.has_right_scopes( self.github.oauth_scopes )
+    
     # If wants to do any repo stuff. Also defines self.owner_name if it's not defined already.
     if value( 'wants_to_set_repo_secrets' ) or value( 'wants_to_set_up_tests' ):
       self.repo = self.get_repo( self.repo_url )
       if self.repo:
-        is_valid_collaborator = self.is_repo_collaborator()
-        self.is_repo_admin()
+        # TODO: Add branch name to confirmation page or final page?
+        # TODO: Allow user to pick a custom branch name or to push to default branch?
+        self.branch_name = self.get_free_branch_name()
+        if self.is_repo_collaborator() and value( 'wants_to_set_repo_secrets' ):
+          self.is_repo_admin()
     
     # Set org secrets
-    is_valid_org_admin = False
     if ( value( 'wants_to_set_org_secrets' )):
       self.org = self.get_org()
       if self.org and user:
-        is_valid_org_admin = self.is_valid_org_admin( user, self.org.login )
-        
-    #has_valid_min_repo_permissions = False
-    #if value( 'wants_to_set_repo_secrets' ):  #
-    #  if self.validate_repo():
-    #    if self.validate_repo_collaborator_auth()  # Minimum permissions required
-    #      has_valid_min_repo_permissions = True
-    #
-    #if has_valid_min_repo_permissions and value( 'wants_to_set_repo_secrets' ):
-    #  if self.validate_repo_admin_auth():
-    #    # Set repo secrets
-    #    pass
-      
-    ## Check if repo exists
-    #self.set_github_info_from_repo_url()
-    #self.repo = self.get_repo()
-    #
-    #if self.repo:
-    #  has_access = self.is_repo_collaborator()
-    #  # TODO: Allow user to pick a custom branch name or to push to default branch
-    #  self.branch_name = self.get_free_branch_name()
-    #
-    ## Give as many errors at once as is possible
-    #if len( self.errors ) > 0:
-    #  return self
+        self.is_valid_org_admin( user, self.org.login )
     
     return self
   
-  def get_org( self ):
-    """Return org if it exists, otherwise None."""
-    # Check if org exists
-    try:
-      # Trigger for authentication error or feedback for the user
-      org = self.github.get_organization( self.owner_name )
-    except Exception as error1:
-      # UnknownObjectException: 404 {"message": "Not Found", "documentation_url": "https://docs.github.com/rest/reference/orgs#get-an-organization"}
-      org = None
-      log( error1.__dict__, 'console' )
-      error1.data[ 'details' ] = self.org_does_not_exist_error
-      self.errors.append( error1 )
-    
-    return org
-    
-  def is_valid_org_admin( self, user, org_name ):
-    """Make sure org auth information is valid."""
-    valid = True
-    # Check if user belongs to org
-    try:
-      membership = user.get_organization_membership( org_name )
-      role = membership.role
-    except Exception as error2:
-      valid = False
-      role = None
-      log( error2.__dict__, 'console' )
-      error2.data[ 'details' ] = self.not_an_org_member_error
-      self.errors.append( error2 )
-
-    # Check if user is admin of org
-    if role != 'admin':
-      valid = False
-      # Show error
-      error3 = ErrorLikeObject( message='Not an admin', details=self.not_org_admin_error )
-      log( error3.__dict__, 'console' )
-      self.errors.append( error3 )
-    
-    return valid
+  def has_right_scopes( self, scopes ):
+    """Make sure the developer gave the token the right scopes"""
+    # TODO: discuss: Really if just setting repo secrets, only need repo permissions, but do we want to make it that complicated/inconsistent?
+    if value('wants_to_set_org_secrets') and value('wants_to_set_up_tests'):
+      has_scopes = "admin:org" in scopes and "workflow" in scopes and "repo" in scopes
+    elif value('wants_to_set_org_secrets') and not value('wants_to_set_up_tests'):
+      has_scopes = "admin:org" in scopes
+    else:
+      has_scopes = "workflow" in scopes and "repo" in scopes
+      
+    if not has_scopes:
+      error = ErrorLikeObject( message='Incorrect Personal Access Token scopes', details=self.github_pat_scopes_error )
+      self.errors.append( error )
+      log( error.__dict__, 'console' )
+      
+    return has_scopes
   
   def get_repo( self, repo_url ):
     """Get repo obj of given repo. Make sure repo exists."""
@@ -198,8 +157,45 @@ class TestInstaller(DAObject):
       error3 = ErrorLikeObject( message='Not an admin', details=self.not_org_admin_error )
       log( error3.__dict__, 'console' )
       self.errors.append( error3 )
-    
     return role != 'admin'
+  
+  def get_org( self ):
+    """Return org if it exists, otherwise None."""
+    # Check if org exists
+    try:
+      # Trigger for authentication error or feedback for the user
+      org = self.github.get_organization( self.owner_name )
+    except Exception as error1:
+      # UnknownObjectException: 404 {"message": "Not Found", "documentation_url": "https://docs.github.com/rest/reference/orgs#get-an-organization"}
+      org = None
+      log( error1.__dict__, 'console' )
+      error1.data[ 'details' ] = self.org_does_not_exist_error
+      self.errors.append( error1 )
+    return org
+    
+  def is_valid_org_admin( self, user, org_name ):
+    """Make sure org auth information is valid."""
+    valid = True
+    # Check if user belongs to org
+    try:
+      membership = user.get_organization_membership( org_name )
+      role = membership.role
+    except Exception as error2:
+      valid = False
+      role = None
+      log( error2.__dict__, 'console' )
+      error2.data[ 'details' ] = self.not_an_org_member_error
+      self.errors.append( error2 )
+
+    # Check if user is admin of org
+    if role != 'admin':
+      valid = False
+      # Show error
+      error3 = ErrorLikeObject( message='Not an admin', details=self.not_org_admin_error )
+      log( error3.__dict__, 'console' )
+      self.errors.append( error3 )
+    
+    return valid
   
   def get_free_branch_name( self ):
     """Return str of valid avialable branch name or None. Add appropriate errors."""
@@ -240,16 +236,18 @@ class TestInstaller(DAObject):
   # All checks should have passed at this point
   ###############################
   def update_github( self ):
-    """Update github with what it needs and make a PR."""
-    self.create_secrets()
-    #self.make_new_branch()
-    #self.push_files()
-    #self.make_pull_request()
+    """If desired, set repo or org secrets. If desired, add test files to repo."""
+    if value( 'wants_to_set_org_secrets' ) or value( 'wants_to_set_repo_secrets' ):
+      self.create_secrets()
+    if value( 'wants_to_set_up_tests' ):
+      self.make_new_branch()
+      self.push_files()
+      self.make_pull_request()
     return self
   
   def create_secrets( self ):
     """Set the GitHub repo secrets the tests need to log into the da server and
-    create projects to contain the interviews being tested. TODO: use PyGithub's secret handling https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html?highlight=secret#github.Repository.Repository.create_secret. Org scope still missing: https://github.com/PyGithub/PyGithub/issues/1373#issuecomment-856616652."""
+    create projects to contain the interviews being tested. TODO: use PyGithub's secret handling ."""
     self.put_secret( 'SERVER_URL', self.server_url )
     self.put_secret( 'PLAYGROUND_EMAIL', self.email )
     self.put_secret( 'PLAYGROUND_PASSWORD', self.password )
@@ -257,15 +255,11 @@ class TestInstaller(DAObject):
     return self
   
   def put_secret( self, secret_name, secret_value ):
-    """Add or update one secret to the GitHub repo."""
+    """Add or update one secret to the GitHub repo. """
     # Encryption by hand in case the lib gets discontinued: https://gist.github.com/plocket/af03ac9326b2ae6d36c937b125b2ea0a
-    # Create repo secret: https://docs.github.com/en/rest/reference/actions#create-or-update-a-repository-secret
     
     if value('wants_to_set_org_secrets'):
-      # No PyGithub secrets for org yet
-      # by hand: https://github.com/PyGithub/PyGithub/blob/master/github/PublicKey.py#L39-L44
-      # https://github.com/PyGithub/PyGithub/blob/master/github/Repository.py#L1420-L1438
-      # https://pygithub.readthedocs.io/en/latest/github_objects/PublicKey.html
+      # PyGithub org secrets still missing: https://github.com/PyGithub/PyGithub/issues/1373#issuecomment-856616652
       headers1, data = self.org._requester.requestJsonAndCheck(
         "GET", f"{ self.org.url }/actions/secrets/public-key"
       )
@@ -281,6 +275,7 @@ class TestInstaller(DAObject):
       )
       
     elif value('wants_to_set_repo_secrets'):
+      # https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html?highlight=secret#github.Repository.Repository.create_secret
       self.repo.create_secret( secret_name, secret_value )
     
     return self
@@ -289,32 +284,32 @@ class TestInstaller(DAObject):
     """Create new branch off the default branch."""
     # Get default branch
     repo = self.repo
-    default_branch_name = repo.default_branch
-    default_branch = repo.get_branch( default_branch_name )
     # Make new branch off of default branch
+    default_branch = repo.get_branch( repo.default_branch )
     ref_path = "refs/heads/" + self.branch_name
     response = repo.create_git_ref( ref_path, default_branch.commit.sha )
     
     return self
   
   def push_files( self ):
-    """Send files to folders in new branch in github.
-    https://pygithub.readthedocs.io/en/latest/examples/Repository.html#create-a-new-file-in-the-repository'''
-    # https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html#github.Repository.Repository.create_file"""
+    """Send files to folders in new branch in github."""
     test_path = 'docassemble/' + self.package_name + '/data/sources/example_test.feature'
     test_commit_message = 'Add ' + test_path
+    self.send_file( '.github/workflows/run_form_tests.yml', 'Add .github/workflows/run_form_tests.yml', self.run_form_tests_str )  # 5
     self.send_file( test_path, test_commit_message, self.example_test_str )  # 2
     self.send_file( '.env_example', 'Add .env_example', self.env_example_str )  # 1
     self.send_file( '.gitignore', 'Add .gitignore', self.gitignore_str )  # 3
     self.send_file( 'package.json', 'Add package.json', self.package_json_str )  # 4
-    self.send_file( '.github/workflows/run_form_tests.yml', 'Add github/workflows/run_form_tests.yml', self.run_form_tests_str )  # 5
     return self
   
   def send_file( self, path, msg, contents ):
-    '''Either create a new file or update an existing file with the given data.
+    """Either create a new file or update an existing file with the given data.
        See https://stackoverflow.com/a/66673303/14144258.
        TODO: Discuss removing `self` to make it data-based.
-       TODO: Shall we include the committer's user name, etc?'''
+       TODO: Shall we include the committer's user name, etc?"""
+    
+    #https://pygithub.readthedocs.io/en/latest/examples/Repository.html#create-a-new-file-in-the-repository'''
+    # https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html#github.Repository.Repository.create_file
     try:
       # Try to create the file
       self.repo.create_file( path, msg, contents, branch=self.branch_name )
@@ -324,11 +319,10 @@ class TestInstaller(DAObject):
         file = self.repo.get_contents( path )
         self.repo.update_file( path, msg, contents, file.sha, branch=self.branch_name )
       else:
-        # An undefined variable will make this error invisible as
-        # da has a special way of dealing with those.
+        # fyi, da will swallow an undefined variable name error
         raise error
     
-    return self;
+    return self
   
   def make_pull_request( self ):
     """Make a pull request with the new branch with changed files.
