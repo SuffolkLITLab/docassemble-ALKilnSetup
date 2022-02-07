02c309ca83d05ee9e24b4ba01d0ef4561927c7e4
plocket <52798256+plocket@users.noreply.github.com>
2022-02-06 21:06:52 -0500

    Setup: Allow dev to include test choosing from list of possible files (#178)
    and stop creating "example.feature" file on setup, removing the use of "test.yml" as the example.
    
    Also:
    
    * add info about disabling tests several places, providing a link to our documentation. The link just goes to the main automated testing page to be most robust in case of changes.
    * add info about what files will be overwritten or created ahead of time (though maybe that info could be in a better place, though at earlier stages we don't know if a test file would be created).
    * add the name ALKiln a bunch of places in the files that are pushed and the PR title and body.
    * shortened some instructions
    * updated package.json for 'colors' lib, etc.
    
    Close https://github.com/SuffolkLITLab/ALKiln/issues/485
    
    * Offer to make a test for the developer with a list of possible files
    
    * Add more info before pushing to GitHub
    
    * Update new text, remove func import

--- docassemble/ALAutomatedTestingTests/al_set_up_testing.py
+++ docassemble/ALAutomatedTestingTests/al_set_up_testing.py
@@ -7,7 +7,7 @@ import codecs
 from base64 import b64encode
 import re
 import json
-from docassemble.base.util import log, value
+from docassemble.base.util import log, value, defined
 from docassemble.base.core import DAObject
 
 # reference:
@@ -295,24 +295,57 @@ class TestInstaller(DAObject):
     
     return self
   
+  def get_question_file_names( self ):
+    """Get the names of the files in the `questions` folder.
+    See https://pygithub.readthedocs.io/en/latest/examples/Repository.html#get-all-of-the-contents-of-the-repository-recursively
+    """
+    ## Don't know how to implement this so it'll recalculate when needed
+    ## Maybe `depends on: installer.github_url`?
+    #if self.hasattr( 'question_file_names' ):
+    #  return self.question_file_names
+    #else:
+    
+    # Get the path to the "questions" folder
+    package_path = ""
+    # Get the (only possible?) folder inside the "docassemble" folder
+    contents = self.repo.get_contents(f"docassemble")
+    for item in contents:
+        if item.type == "dir":
+            package_path = f"{ item.path }/data/questions"
+
+    # Get the names of the files in the "questions" folder
+    question_files = self.repo.get_contents( package_path )
+    names = []
+    for file in question_files:
+      names.append( file.name )
+    
+    #self.question_file_names = names
+    return names
+  
   def push_files( self ):
-    """Send files to folders in new branch in github."""
-    test_path = 'docassemble/' + self.package_name + '/data/sources/example_test.feature'
-    test_commit_message = 'Add ' + test_path
-    self.send_file( test_path, test_commit_message, self.example_test_str )  # 2
-    self.send_file( '.env_example', 'Add .env_example', self.env_example_str )  # 1
-    self.send_file( '.gitignore', 'Add .gitignore', self.gitignore_str )  # 3
-    self.send_file( 'package.json', 'Add package.json', self.package_json_str )  # 4
-    self.send_file( '.github/workflows/run_form_tests.yml', 'Add .github/workflows/run_form_tests.yml', self.run_form_tests_str )  # 5
+    """Push each file to the new branch in github in the correct directory."""
+    # Only push test if they wanted it
+    if self.test_files_wanted.any_true():
+      test_path = 'docassemble/' + self.package_name + '/data/sources/interviews_run.feature'
+      test_commit_message = 'Add ' + test_path + ' for ALKiln automated tests'
+      self.send_file( test_path, test_commit_message, self.first_feature_file_str )  # 1
+      
+    # Push the mandatory files
+    self.send_file( '.env_example', 'Add .env_example for ALKiln automated tests', self.env_example_str )  # 2
+    self.send_file( '.gitignore', 'Add .gitignore for ALKiln automated tests', self.gitignore_str )  # 3
+    self.send_file( 'package.json', 'Add package.json for ALKiln automated tests', self.package_json_str )  # 4
+    self.send_file( '.github/workflows/run_form_tests.yml', 'Add .github/workflows/run_form_tests.yml for ALKiln automated tests', self.run_form_tests_str )  # 5
+    
     return self
   
   def send_file( self, path, msg, contents ):
-    """Either create a new file or update an existing file with the given data.
+    """Either create a new file or update an existing file with the given data
+       and push it to new branch.
        See https://stackoverflow.com/a/66673303/14144258.
        TODO: Discuss removing `self` to make it data-based.
-       TODO: Shall we include the committer's user name, etc?"""
+       TODO: Shall we remove the committer's user name/email?"""
     
-    #https://pygithub.readthedocs.io/en/latest/examples/Repository.html#create-a-new-file-in-the-repository'''
+    #https://pygithub.readthedocs.io/en/latest/examples/Repository.html#create-a-new-file-in-the-repository
     # https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html#github.Repository.Repository.create_file
     try:
       # Try to create the file
@@ -334,14 +367,19 @@ class TestInstaller(DAObject):
     # TODO: Check mergability of a PR?
     base_name = self.repo.default_branch
     head_name = self.branch_name
-    title = 'Update to automated tests'  # TODO: Add issue # if desired
-    description = '''Updates:
-- [x] .env_example
-- [x] tests/features/example_test.feature
-- [x] .gitignore
-- [x] package.json
-- [x] .github/workflows/run_form_tests.yml
-'''  # TODO: Add issue # if desired
+    title = 'Add ALKiln automated tests'  # TODO: Add issue # if desired
+    description = '''Added these files:'''
+    if self.test_files_wanted.any_true():
+      description += '''
+- tests/features/interviews_run.feature'''
+    description += '''
+- .github/workflows/run_form_tests.yml
+- .env_example
+- .gitignore
+- package.json
+
+Want to disable the tests? See documentation for ALKiln tests at https://suffolklitlab.github.io/docassemble-AssemblyLine-documentation/docs/automated_integrated_testing.
+'''
     response = self.repo.create_pull(base=base_name, head=head_name, title=title, body=description)
     self.pull_url = response.html_url
     
