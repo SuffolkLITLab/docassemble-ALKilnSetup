c0d2dcaa4bbd6bd937caac1c6457a4ce0372179c
plocket <52798256+plocket@users.noreply.github.com>
2023-07-04 13:11:41 -0400

    Address #216, switch repo name

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
                 out.setdefault(package, []).append(prefix+name)
     return out
 
-setup(name='docassemble.ALAutomatedTestingTests',
+setup(name='docassemble.ALKilnSetup',
       version='1.0.1',
       description=('A docassemble extension for testing the AssemblyLine automated integrated testing framework.'),
       long_description='# ALKiln support package\r\n\r\nAssembly Line Kiln (ALKiln) is a framework for automatically testing **any** [docassemble](https://docassemble.org/) package using GitHub. ALKiln is being developed as part of the SuffolkLITLab Document Assembly Line project. See [documentation for kiln](https://suffolklitlab.github.io/docassemble-AssemblyLine-documentation/docs/automated_integrated_testing).\r\n\r\nThis package supports ALKiln in different ways.\r\n\r\n## Tool to set up testing\r\n\r\n[Tap here to set up automated integrated testing for your docassemble interview](https://apps-dev.suffolklitlab.org/start/test-setup/).\r\n\r\nThis repo contains a step-by-step form that a developer can use to set up automated integrated testing for **any** docassemble package. It requires, among other things, a docassemble account and a temporary GitHub personal access token with correct permissions. It will add necessary GitHub secrets, make a new branch, push necessary files to that branch, and make a PR with that branch.\r\n\r\n## Test ALKiln itself\r\n\r\nThis package provides files that the ALKiln testing framework can use to test its own functionality. That includes tests for setting different types of fields, for observing the state of the page, and for getting error messages.\r\n',
@@ -56,6 +56,6 @@ setup(name='docassemble.ALAutomatedTestingTests',
       namespace_packages=['docassemble'],
       install_requires=['PyGithub>=1.59.0', 'PyNaCl>=1.5.0', 'docassemble.ALToolbox>=0.8.1', 'requests>=2.31.0'],
       zip_safe=False,
-      package_data=find_package_data(where='docassemble/ALAutomatedTestingTests/', package='docassemble.ALAutomatedTestingTests'),
+      package_data=find_package_data(where='docassemble/ALKilnSetup/', package='docassemble.ALKilnSetup'),
      )
 
